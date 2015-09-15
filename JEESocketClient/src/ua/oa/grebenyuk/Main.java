e33f922fd9cb46e43f079eefe3905d3e8f977336
vyacheslav-grebenyuk <grebenyuk@gmail.com>
2015-09-15 20:09:04 +0300

    Initial commit

---
+++ JEESocketClient/src/ua/oa/grebenyuk/Main.java
@@ -0,0 +1,43 @@
+package ua.oa.grebenyuk;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Main {
+
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		Socket socket = new Socket("127.0.0.1", 4040);
+        InputStream is = socket.getInputStream();
+        OutputStream os = socket.getOutputStream();
+
+        PrintStream writer = new PrintStream(os, true);
+
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(is));
+
+        BufferedReader consoleReader = new BufferedReader(
+                new InputStreamReader(System.in));
+        while (true){
+            String input = consoleReader.readLine();
+
+            System.out.println("input message " + input);
+            writer.println(input);
+
+            if("exit".equals(input)){
+                break;
+            }
+
+            String output = reader.readLine();
+            System.out.println("output message " + output);
+        }
+        socket.close();
+    }
+}
